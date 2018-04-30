00e164116076b95a412893e9d40d926d63a98c28
Cory Pavitt <corypavitt@hotmail.com>
2018-04-30 14:34:24 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+function theBeatlesPlay(musicians, instruments) {
+    var i = [];
+    for(i = 0; i < musicians.length; i++) {
+      var newstring = []
+      newstring.push = `${musicians} plays ${instruments}`'
+    }
+    
+}
