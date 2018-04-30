2940ec65b20593fa4533663355f0d381f72525ab
Cory Pavitt <corypavitt@hotmail.com>
2018-04-30 14:58:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
     var newstring = [];
-    for(var i = 0; i < musicians.length; i++) {
-
-      newstring.push(`${musicians[i]} plays ${instruments}`)
+    for(var i = 0; i < musicians.length; i++ | var x = 0; x< instruments.length; x++) {
+      newstring.push(`${musicians} plays ${instruments}`)
     }
 return newstring
 }
